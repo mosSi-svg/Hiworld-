0d2fa5f872f623db5aa76928171eb6feffac650a
mosSi-avg <1005801715@qq.com>
2019-10-17 17:49:36 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -3,6 +3,9 @@ public class HiWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello ! World!");
+        System.out.println("Hello ! ");
+        System.out.println("Hello ! ");
+        System.out.println("Hello ! ");
+        System.out.println("Hello ! ");
     }
 }
